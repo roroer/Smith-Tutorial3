8d15e4e3ea1eb5b1f9428a21ea70694ba9b6a3ec
roroer <meeproroer@gmail.com>
2019-12-07 22:34:04 -0500

    final

--- Assets/Scripts/BGScroller.cs
+++ Assets/Scripts/BGScroller.cs
@@ -4,32 +4,67 @@ using UnityEngine;
 
 public class BGScroller : MonoBehaviour
 {
-	public float scrollSpeed;
-	public float tileSizeZ;
-	public float winScrollSpeed;
-	public float loseScrollSpeed;
-	public float smoothSpeed = 4;
-
 	public GameController gameController;
 
-	private Vector3 startPosition;
+	public float normalSpeed;
+	public float winSpeed;
+	public float loseSpeed;
+	private float currentSpeed;
+	public float tileLength;
+
+	private Vector3 startPos;
 
 	private void Start() {
-		startPosition = transform.position;
+		startPos = transform.position;
 		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
 		if (gameControllerObject != null) {
 			gameController = gameControllerObject.GetComponent<GameController>();
 		}
 	}
-	void Update()
-    {
-		float newPos =  Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
-		transform.position = startPosition + Vector3.forward * newPos;
-		if(gameController.win == true) {
-			scrollSpeed = Mathf.Lerp(scrollSpeed, winScrollSpeed, Time.deltaTime * smoothSpeed);
+
+	private void Update() {
+		if (gameController.win == true) {
+			currentSpeed = Mathf.Lerp(normalSpeed, winSpeed, 0.5f);
+		} else if (gameController.lose == true) {
+			currentSpeed = Mathf.Lerp(normalSpeed, loseSpeed, 0.5f);
+		} else {
+			currentSpeed = normalSpeed;
 		}
-		if (gameController.lose == true) {
-			scrollSpeed = Mathf.Lerp(scrollSpeed, loseScrollSpeed, Time.deltaTime * smoothSpeed);
+		transform.position -= transform.up * currentSpeed * Time.deltaTime;
+		if (transform.position.z <= startPos.z - tileLength) {
+			transform.position = startPos;
 		}
 	}
+
+
+
+	/*	public float scrollSpeed;
+		public float tileSizeZ;
+		public float winScrollSpeed;
+		public float loseScrollSpeed;
+		public float smoothSpeed = 0.2f;
+
+		public GameController gameController;
+
+		private Vector3 startPosition;
+
+		private void Start() {
+			startPosition = transform.position;
+			GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+			if (gameControllerObject != null) {
+				gameController = gameControllerObject.GetComponent<GameController>();
+			}
+		}
+		void Update()
+		{
+			float newPos =  Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+			transform.position = startPosition + Vector3.forward * newPos;
+			if(gameController.win == true) {
+				scrollSpeed = Mathf.Lerp(scrollSpeed, winScrollSpeed, smoothSpeed);
+			}
+			if (gameController.lose == true) {
+				scrollSpeed = Mathf.Lerp(scrollSpeed, loseScrollSpeed, smoothSpeed);
+			} 
+		} */
+
 }
